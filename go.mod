e059e06d8dd5dc1f8a87b2177901b5a92dd76a69
DouglasVolcato <douglasvolcato@gmail.com>
2025-03-01 20:55:39 -0300

    chore: config hot reload setup

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module minha-api
+module api
 
 go 1.24.0
 
--- main.go
+++ main.go
@@ -10,9 +10,9 @@ func main() {
 	os.Setenv("PORT", "9090")
 
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"message": "Api is running",
 		})
 	})
 	r.Run()
