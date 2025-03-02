e3f24ca7e15511ed3d1264fbe2b5de2fea69a759
DouglasVolcato <douglasvolcato@gmail.com>
2025-03-02 19:19:49 -0300

    refactor: add type generics

--- src/presentation/controllers/controller.go
+++ src/presentation/controllers/controller.go
@@ -1,44 +1,41 @@
 package controllers
 
 import (
+	"app/src/domain/usecases"
 	"app/src/infra/database"
 	"database/sql"
 )
 
-type Controller struct {
-	usecaseFunc func(data interface{}) (interface{}, error)
+type Controller[I any, O any] struct {
+	usecase *usecases.UseCase[I, O]
 }
 
-func NewController(usecaseFunc func(data interface{}) (interface{}, error)) *Controller {
-	return &Controller{usecaseFunc: usecaseFunc}
+func NewController[I any, O any](usecase *usecases.UseCase[I, O]) *Controller[I, O] {
+	return &Controller[I, O]{usecase: usecase}
 }
 
-func (controller *Controller) Handle(databaseConnection *sql.DB, data interface{}) interface{} {
+func (controller *Controller[I, O]) Execute(databaseConnection *sql.DB, data I) (O, error) {
 	var transaction = database.NewTransaction(databaseConnection)
 	var err error
 
 	err = transaction.BeginTransaction()
 	if err != nil {
-		return struct {
-			error string
-		}{error: "Falha ao iniciar transação"}
+		var empty O
+		return empty, err
 	}
 
-	response, err := controller.usecaseFunc(data)
-
+	response, err := controller.usecase.Execute(transaction, data)
 	if err != nil {
 		transaction.RollbackTransaction()
-		return struct {
-			error string
-		}{error: err.Error()}
+		var empty O
+		return empty, err
 	}
 
 	err = transaction.CommitTransaction()
 	if err != nil {
-		return struct {
-			error string
-		}{error: "Falha ao finalizar transação"}
+		var empty O
+		return empty, err
 	}
 
-	return response
+	return response, nil
 }
