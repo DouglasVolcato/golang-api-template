1d61c4ee8cfc34e3e76ef1b9c7dc9903f33a08c8
DouglasVolcato <douglasvolcato@gmail.com>
2025-03-04 16:45:07 -0300

    chore: add repository extension exemple

--- src/infra/repositories/base_repository.go
+++ src/infra/repositories/base_repository.go
@@ -2,13 +2,37 @@ package repositories
 
 import (
 	"app/src/infra/database"
+	"fmt"
+
+	"database/sql"
 )
 
-var BaseRepository = database.NewRepository(
-	"base_table",
-	"id",
-	[]string{"id", "name", "created_at", "updated_at"},
-	[]string{"id", "name", "created_at"},
-	[]string{"id", "name"},
-	[]string{"name"},
-)
+var BaseRepository = baseRepository()
+
+type repositoryType struct {
+	*database.Repository
+}
+
+func baseRepository() *repositoryType {
+	return &repositoryType{
+		Repository: database.NewRepository(
+			"base_table",
+			"id",
+			[]string{"id", "name", "created_at", "updated_at"},
+			[]string{"id", "name", "created_at"},
+			[]string{"id", "name"},
+			[]string{"name"},
+		),
+	}
+}
+
+func (repo *repositoryType) SelectCountAll(transaction *database.Transaction, limit int, offset int) (*sql.Rows, error) {
+	return database.ExecuteQuery(
+		transaction,
+		fmt.Sprintf(
+			"SELECT *, (SELECT COUNT(*) FROM base_table) FROM base_table LIMIT %d OFFSET %d",
+			limit,
+			offset,
+		),
+	)
+}
